cb30c277665018ea1dfad9ef2a42360d40f953ec
shu_0115 <raisondetre0115@gmail.com>
2012-04-08 20:28:06 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luti::Application.config.secret_token = 'f4e152c3a84e8e9be6d1182d84c607a4ee1938390c5475b998d23c5da97383a27d90118bf9b7d02a5aa1a5376dea9000387998db32a6d8a3e7bd38ab06759c2f'
